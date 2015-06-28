1526ba3ea79aa4e4994cb08a2a53b3ce751ea23e
Zeeshan Rasool <zshnr@zshnr.com>
2015-06-28 05:18:10 +0100

    added a noStock property on component. for an out of stock item, the add to cart button cannot be pressed

--- app/components/individual-product.js
+++ app/components/individual-product.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  noStock: function() {
+    return this.get('product').get('quantityInStock') === 0;
+  }.property('product.quantityInStock')
 });

47ddf18b8f20013eaca32d49f2344465b649c69c
Zeeshan Rasool <zshnr@zshnr.com>
2015-07-01 20:56:14 +0100

    addToCart button bubbles up from component to route

--- app/routes/products.js
+++ app/routes/products.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model:function(){
       return this.store.findAll('product');
+  },
+  actions: {
+    addToCart: function(product) {
+      console.log(product.get('name'));
+    }
   }
 });

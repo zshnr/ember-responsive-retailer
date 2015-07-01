81e55033bdcf847a89b321730b4e61a1f5d82eb6
Zeeshan Rasool <zshnr@zshnr.com>
2015-07-01 21:00:04 +0100

    addToCart button decreases quantityInStock property of the object but does not persist the result. Next step is to save this to the mock database so that users cannot refresh and reset stock levels on the page

--- app/routes/products.js
+++ app/routes/products.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   },
   actions: {
     addToCart: function(product) {
-      console.log(product.get('name'));
+      product.decrementProperty('quantityInStock');
     }
   }
 });
